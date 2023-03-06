46e841ff604d75ec55e19823c57d6b063a84af7d
david betech <betech35@gmail.com>
2023-03-06 16:52:49 -0600

    challenge done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,28 @@
+require "json"
+require "open-uri"
+
+
 class GamesController < ApplicationController
 
   def new
-    @letters = ('a'..'z').to_a
+    random_letters = ('a'..'z').to_a
+    @letters = random_letters.shuffle.take(10)
   end
 
   def score
+    @word = (params[:word])
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{@word}")
+    json = JSON.parse(response.read)
+    @real_word = json["found"]
+
+    if @real_word == true
+      @score = "congratulations! #{@word} does exist"
+    else
+      @score = "sorry but #{@word} does not seem to be a valid word"
+    end
+
   end
+
+
+
 end
